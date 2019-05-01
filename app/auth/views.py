cad00d5cd19cf55a1a4882f7ffb5d8cf1c2ab378
Derrick K <derrickwaweru@gmail.com>
2019-05-01 20:33:54 +0300

    add views modules

---
+++ app/auth/views.py
@@ -0,0 +1,45 @@
+from flask import render_template, redirect, url_for, flash, request, session
+from flask_login import login_user, logout_user, login_required
+from . import auth
+from ..models import User, Post
+from .forms import RegistrationForm, LoginForm
+from .. import db
+from ..email import mail_message
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+  User.init_db()
+  Post.default_posts()
+  login_form = LoginForm()
+  if login_form.validate_on_submit():
+    user = User.query.filter_by(username=login_form.username.data).first()
+    session['username']=login_form.username.data
+    if user is not None and user.verify_password(login_form.password.data):
+      login_user(user, login_form.remember.data)
+      return redirect(request.args.get('next') or url_for('main.index'))
+
+    flash('Invalid Username or Password')
+
+  title = 'Login'
+  return render_template('auth/login.html', login_form = login_form, title = title)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(email=form.email.data, username=form.username.data, password=form.password.data, firstname=form.fname.data, lastname=form.lname.data)
+
+    User.save_user(user)
+
+    mail_message('Welcome to Blogi', 'email/welcome_user', user.email, user=user)
+    return redirect(url_for('auth.login'))
+    title='New Account'
+
+  return render_template('auth/register.html', registration_form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+  logout_user()
+  return redirect(url_for('auth.login'))
+
---
+++ app/main/views.py
@@ -0,0 +1,90 @@
+from flask import render_template, request, redirect, url_for, abort, flash, session
+from . import main
+from ..models import User, Post, Comments
+from ..date_pipe import date_calc
+from flask_login import login_required, current_user
+from .. import db
+from .forms import PostForm, CommentForm
+from datetime import datetime
+from functools import wraps
+from ..email import updates_mail_message
+
+def requires_admin(access_level):
+  def decorator(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+      if not session.get('username'):
+        return redirect(url_for('auth.login'))
+      user = User.find_by_username(session['username'])
+      if not user.allowed(access_level):
+        return redirect(url_for('main.index', message='You do not have the required permissions to access that'))
+      return f(*args, **kwargs)
+    return decorated_function
+  return decorator
+
+@main.route('/')
+@login_required
+def index():
+  posts=Post.query.all()
+  title='Home - Blogi'
+  return render_template('index.html', title=title, posts=posts)
+
+@main.route('/profile/user/<uname>/<id>')
+def profile_user(id, uname):
+  user = User.query.filter_by(username = uname).first()
+  memberFor=date_calc(user.joined)
+  return render_template('profile/profile.html', memberFor=memberFor)
+
+@main.route('/profile/admin/<uname>/<id>')
+def profile_admin(id, uname):
+  nō_users=User.query.all()
+  nō_posts=Post.query.all()
+  return render_template('profile/admin.html', users=nō_users, posts=nō_posts)
+
+@main.route('/writing-posts', methods=['GET', 'POST'])
+@login_required
+def write_post():
+  users=User.query.all()
+  form = PostForm()
+  if form.validate_on_submit():
+    post = Post(title=form.title.data, body=form.body.data)
+
+    Post.save_post(post)
+    return redirect(url_for('main.index'))
+
+    update_mail_message('Update from Derrick', 'email/update_email', users.email)
+
+  title='New Blog Post'
+  return render_template('new_post.html', post=form, title=title)
+
+@main.route('/view_comments/<id>')
+def view_comments(id):
+  user = User.query.filter_by(id=id).first()
+  post = Post.query.filter_by(id=id).first()
+  comments = Post.get_comments(post)
+
+  return render_template('comments.html', comments=comments, post=post, user=user)
+
+@main.route('/write-comment/<id>', methods=['GET', 'POST'])
+def write_comments(id):
+  form=CommentForm()
+  post = Post.query.filter_by(id=id).first()
+  user = User.query.filter_by(id=id).first()
+  if form.validate_on_submit():
+    comment = Comments(comment=form.comment.data, user=user, post_comments=post)
+    comment.save_comment()
+
+    return redirect(url_for('main.index'))
+
+  return render_template('comment.html', comment=form, user=user, post=post)
+
+@main.route('/delete-comments/<id>', methods=['GET', 'POST'])
+def delete_comments(id):
+  post = Post.query.filter_by(id=id).first()
+  comment = Post.query.filter_by(post=id).first()
+  user = User.query.filter_by(id=id).first()
+  
+  if user.access>0:
+    return delete_comments(comment)
+  
+  return render_template('comment.html', comment=comments, post=post, user=user)
