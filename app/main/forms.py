b59e09870aa4268b0760546b330fd28364e78a5a
Derrick K <derrickwaweru@gmail.com>
2019-05-01 20:33:28 +0300

    add forms

---
+++ app/main/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+class PostForm(FlaskForm):
+
+  title=StringField('Blog Title')
+  body=TextAreaField('Blog Body')
+  submit=SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+  
+  comment=TextAreaField('Your comment:')
+  submit=SubmitField('Post')
